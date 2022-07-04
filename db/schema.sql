7c2defaa550d9b2a60c60b75be90b2b08994999c
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-07-04 17:18:56 -0500

    working seeds and tables shown in mysql'

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,37 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+SET GLOBAL FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
-    id INT OT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id)
-    FOREIGN KEY (manager_id)
-    REFERENCES employees(id)
-);
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     department_id INT,
     salary DECIMAL NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
