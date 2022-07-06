9874c5b1597d23067fbd97d4d383f99f6c57a9cc
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-07-06 08:45:16 -0500

    fixed config file and server

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const connection = mysql.createConnection(
     {
         host: 'localhost',
-        user: process.env.DB_NAME,
+        user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_USER
+        database: process.env.DB_NAME
     }
 );
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer');
 const connection = require('./config/connection');
 const cTable = require('console.table');
 const mysql = require('mysql2');
-const connection = require('./config/connection');
 
 
 connection.connect(err => {
@@ -43,11 +42,11 @@ const renderQuestions = () => {
         if(response. selection === 'View All Departments') {
 
             const sql = `SELECT department.id AS 'ID', 
-                        department.department_name AS 'Department' FROM department;`
+                        department.department_name AS 'Department' FROM department`
 
             connection.query(sql, (err, rows) => {
                 if(err) {
-                    console.log(err)
+                    console.log(err);
                 }
                 console.table(rows);
                 renderQuestions();
@@ -58,21 +57,119 @@ const renderQuestions = () => {
         if (response.selection === 'View All Roles') {
 
             const sql = `SELECT roles.id AS 'ID', 
-                        roles.title AS 'Title, 
+                        roles.title AS 'Title', 
                         department.department_name AS 'Department',
                         roles.salary AS 'Salary'
 
                         FROM roles
                         
-                        JOIN department ON roles.department_id = department.id`
+                        JOIN department ON roles.department_id = department.id`;
 
             connection.query(sql, (err, rows) => {
                 if(err) {
-                    console.log(err)
+                    console.log(err);
                 }
-                console.table(rows)
+                console.table(rows);
                 renderQuestions();
             })
         }
+
+        if (response.selection === 'View All Employees') {
+
+            const sql = `SELECT employee.id AS 'ID', 
+                        employee.first_name AS 'First Name', 
+                        employee.last_name AS 'Last Name',
+                        roles.title AS 'Title',
+                        department.department_name AS 'Department',
+                        roles.salary AS 'Salary',
+                        CONCAT (manager.first_name, ' ', manager.last.name) AS 'Manager',
+
+                        FROM employee
+                        
+                        LEFT JOIN roles ON (employee.roles_id = roles.id)
+                        LEFT JOIN department ON (department.id = roles.department_id)
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id;`
+
+            connection.query(sql, (err, rows) => {
+                if(err) {
+                    console.log(err);
+                }
+                console.table(rows);
+                renderQuestions();
+            })
+        }
+
+        if(response.selection === 'Add Department') {
+            inquirer.prompt ([
+                {
+                    type: 'input',
+                    name: 'deptName',
+                    message: 'Enter Department Name'
+                }
+
+            ])
+            .then(answer => {
+                const sql = `INSERT INTO department (department_name) VALUES ('${answer.deptName}')`;
+
+                connection.query(sql, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(`${answer.deptName} added`);
+                    renderQuestions();
+                });
+            })
+        };
+
+        if (response.selection === 'Add Role') {
+            
+            const sql = `SELECT * FROM department`;
+
+            connection.query(sql, (err, data) => {
+                if(err) {
+                    console.log(err)
+                }
+
+                const dept = data.map(({ id, department_name }) => ({ name: department_name, value: id}))
+
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'title',
+                        message: 'Enter Role'
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'Enter salary for the specifed role'
+                    },
+                    {
+                        type: 'list',
+                        name: 'newDept',
+                        message: 'Which department does this role belong to?',
+                        choices: dept
+                    }
+                ])
+                .then(answer => {
+                    const { title, salary, newDept } = answer;
+
+                    const sqlAnswers = `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', '${newDept}')`;
+
+                    connection.query(sqlAnswers, (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        console.log(`${title} added`);
+                        renderQuestions();
+                    });
+                })
+            })
+        }
+
+        if(response.selection === 'Add Employee') {
+
+        }
 })
+
 };
